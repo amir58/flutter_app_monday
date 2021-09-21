88c83ba2f7838fb4793bea3a023073cc0faaf1e1
amir58 <devamir11@gmail.com>
2021-09-21 18:59:20 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/net/flutter_app_monday/MainActivity.kt
@@ -0,0 +1,6 @@
+package net.flutter_app_monday
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/basics.dart
@@ -0,0 +1,28 @@
+void main() {
+
+  print(1);
+  print(2.5);
+  print('amir');
+
+  // data types : String , int , double , bool ( boolean )
+  // variables => dataType variableName = value ;
+
+  String name = 'Amir Mohammed';
+
+  int floorNumber = 8 ;
+
+  bool isWorking = true;
+
+  print(name);
+  print(floorNumber);
+  print(isWorking);
+
+  // naming => a-z , $ , _
+
+  print('My name is ' + name);
+  print('My name is $name');
+
+  // print("My name is : " + name + " , is working : " + isWorking + " , working in floor number " + floorNumber);
+  print('My name is : $name , is working : $isWorking , working in floor number $floorNumber');
+
+}
